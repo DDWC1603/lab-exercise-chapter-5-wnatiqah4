d056bd3ad16b62b5e41c7a27a6b7db7c9044f29d
wnatiqah4 <33883982+wnatiqah4@users.noreply.github.com>
2018-01-16 10:46:11 +0800

    task completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,5 +1,17 @@
-//this is an unfinish program
-//finish up this program
+//student name : WAN NURUL ATIQAH
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int x=3,y=2;
+    
+    cout<<"x = "<<x<<" y = "<<y<<endl;
+    cout<<x+y;
+    getch();
+}
 
 int sum(int x,int y)
 {
